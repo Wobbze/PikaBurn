52a4277aeea5a8881520ad155ad69f1f114ac5f7
Wobbze <163325284+Wobbze@users.noreply.github.com>
2024-04-25 12:27:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from discord.ext import commands
 import json
 import urllib.request
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Define the necessary intents
 intents = discord.Intents.default()
 intents.messages = True
-intents.message_content = True 
+intents.message_content = True  # Ensure this is enabled for commands to function
 
 token = os.getenv("BOT_TOKEN")
 if not token:
@@ -25,7 +28,7 @@ if not api_key:
     raise ValueError("API_TOKEN environment variable not set!")
 
 # Initial supply of the tokens
-initial_supply = 50000000000  #initial supply was 50,000,000,000
+initial_supply = 50000000000  # Assuming the initial supply was 50,000,000,000
 
 def get_total_supply(token_address, api_key):
     url = f"https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress={token_address}&apikey={api_key}"
